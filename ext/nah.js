c9272d02e13acb8f85092a4258e90f23f3a177d0
Prabhnoor Hans <noor.hans1@gmail.com>
2023-04-29 00:46:49 -0700

    squircle mess

--- ext/nah.js
+++ ext/nah.js
@@ -1,109 +1,137 @@
-// NEW 3
+let GBL_micActivated = false;
 
-const squircle = document.createElement("div");
-squircle.setAttribute("class", "squircle");
-document.body.appendChild(squircle);
+if ("paintWorklet" in CSS) {
+	CSS.paintWorklet.addModule(
+		"https://www.unpkg.com/css-houdini-squircle/squircle.min.js"
+	);
+}
+
+// Contains the buttons and chatBox
+const chatWrapper = document.createElement("div");
+chatWrapper.setAttribute("id", "chatWrapper");
+
+const chatBtnsWrapper = document.createElement("div");
+chatBtnsWrapper.setAttribute("id", "chatBtnsWrapper");
 
 
+// View Button
+const viewSquircle = document.createElement("div");
+viewSquircle.setAttribute("class", "squircleBtn viewBtn");
 
+const viewImg = document.createElement("img");
+viewImg.src = chrome.runtime.getURL("view.png");
+// viewImg.onclick = function() {
+// 	if (chatBox.style.display == "") {
+// 		chatBoxBlur.style.display = "none";
+// 		chatBox.style.display = "none";
+// 	} else {
+// 		chatBox.style.display = "";
+// 		chatBoxBlur.style.display = "none";
+// 	}
+// }
+viewSquircle.appendChild(viewImg);
+chatBtnsWrapper.appendChild(viewSquircle);
 
-let micActivated = false;
+// Mic Button
+const micSquircle = document.createElement("div");
+micSquircle.setAttribute("class", "squircleBtn micBtn");
+
+const micImg = document.createElement("img");
+micImg.src = chrome.runtime.getURL("mic.png");
+// // micImg.onclick = function() {
+// // 	if (micActivated != true) {
+// // 		audio("");
+// // 	}
+// // }
+micSquircle.appendChild(micImg);
+chatBtnsWrapper.appendChild(micSquircle);
+
+chatWrapper.appendChild(chatBtnsWrapper);
 
 const chatBoxWrapper = document.createElement("div");
 chatBoxWrapper.setAttribute("id", "chatBoxWrapper");
+chatBoxWrapper.style.display = "none";
 
-const chatBox = document.createElement("div");
-chatBox.setAttribute("id", "chatBox");
-chatBoxWrapper.appendChild(chatBox);
+// Blur Background
+const chatBoxBlur = document.createElement("div");
+chatBoxBlur.setAttribute("id", "chatBoxBlur");
+chatBoxWrapper.appendChild(chatBoxBlur);
+// chatBoxBlur.style = "--squircle-smooth: .9";
 
-const micBtn = document.createElement("img");
-micBtn.setAttribute("id", "micBtn");
-micBtn.src = chrome.runtime.getURL("mic.png");
-micBtn.onclick = function() {
-	if (micActivated != true) {
-		audio();
+// Border
+const chatBoxBorder = document.createElement("div");
+chatBoxBorder.setAttribute("id", "chatBoxBorder");
+chatBoxWrapper.appendChild(chatBoxBorder);
+
+const chatBoxContent = document.createElement("div");
+chatBoxContent.setAttribute("id", "chatBoxContent");
+chatBoxWrapper.appendChild(chatBoxContent);
+
+chatWrapper.appendChild(chatBoxWrapper);
+
+document.body.appendChild(chatWrapper);
+
+function addMessage(text, from="user", createNew=false)
+{
+	if (createNew)
+	{
+		document.getElementById("chatBoxContent").innerHTML += "<div class='"+from+"Chat chatBubble'>"+text+"</div>";
 	}
-}
-squircle.appendChild(micBtn);
-
-const viewBtn = document.createElement("img");
-viewBtn.setAttribute("id", "viewBtn");
-viewBtn.src = chrome.runtime.getURL("smart.png");
-viewBtn.onclick = function() {
-	if (chatBox.style.display == "") {
-		chatBox.style.display = "none";
-	} else {
-		chatBox.style.display = "";
+	else
+	{
+		document.getElementById("chatBoxContent").lastChild.innerHTML = text;
 	}
-}
-document.body.appendChild(viewBtn);
 
-function userChatUI() {
-	const userChat = document.createElement("div");
-	userChat.setAttribute("class", "userChat");
-	chatBox.appendChild(userChat);
+	chatBoxBlur.style.height = chatBoxContent.offsetHeight + "px";
+	chatBoxBorder.style.height = chatBoxContent.offsetHeight + "px";
 }
 
-function aiChatUI() {
-	const aiChat = document.createElement("div");
-	aiChat.setAttribute("class", "aiChat");
-	chatBox.appendChild(aiChat);
-}
-
-function updateTXT(txt) {
-	document.getElementById("chatBox").lastChild.innerHTML = txt;
-}
+// chatBoxWrapper.style.display = "block";
+// addMessage('Recording1...', from="user", createNew=true)
+// addMessage('Recording2...', from="ai", createNew=true)
+// addMessage('Recording3...', from="user", createNew=true)
+// addMessage('Recording4...', from="ai", createNew=true)
+// addMessage('Recording5...', from="user", createNew=true)
 
 document.body.addEventListener('contextmenu', function(e) {	
-	if (!e.altKey) {
-		return false;
-	}
-	
+	if (!e.altKey) return false;
+
 	e.preventDefault();
-	const textVal = e.target.innerText;
+	let userQuery = e.target.innerText;
 
-	if (!textVal) {
-		return false;
-	}
+	if (!userQuery) return false;
 
-	// Displaying element selected
+	// Highlighting the Selected Text:
 	if (document.getElementById("selected") != null) {
 		document.getElementById("selected").parentElement.innerHTML = document.getElementById("selected").innerHTML;
 	}
-
-	// var newSpan = document.createElement("span");
-	// newSpan.setAttribute("id", "selected");
-	// newSpan.innerHTML = textVal;
-	// e.target.replaceChild(newSpan, e.target.firstChild);
-
-	// e.target.innerHTML = "<span id='selected'>" + e.target.innerHTML + "</span>";
 	e.target.innerHTML = "<mark id='selected'>" + e.target.innerHTML + "</mark>";
 
-	let userAction = "";
+	chatBoxWrapper.style.display = "block";
 
-	if (document.getElementById("chatBoxWrapper") == null) {
-		document.body.appendChild(chatBoxWrapper);
-		console.log(chatBox.style.display);
-	}
+	// addMessage('hello')
+	// addMessage('hi', 'ai')
+	// addMessage('whats up')
 
-	navigator.mediaDevices.getUserMedia({ audio: true, })
+	// -------
+	navigator.mediaDevices
+	.getUserMedia({ audio: true, })
 	.then((stream) => {
-		console.log(stream);
-		mediaRecorder = new MediaRecorder(stream);
+		// console.log(stream);
+		let mediaRecorder = new MediaRecorder(stream);
 		mediaRecorder.start();
-		micActivated = true;
+		// micActivated = true;
 
 		// Displaying
-		userChatUI();
-		viewBtn.style.visibility = "visible";
-		micBtn.style.visibility = "visible";
-		updateTXT("Recording");
-		chatBox.scrollTop = chatBox.scrollHeight;
-		// return;
+		addMessage('Recording...', from="user", createNew=true)
+		// userChatUI();
+		// updateTXT("Recording");
+		// chatBox.scrollTop = chatBox.scrollHeight;
+		// console.log(chatBox.offsetHeight);
+		// chatBoxBlur.style.height = chatBox.offsetHeight;
 
 		mediaRecorder.ondataavailable = e => {
-			
-			updateTXT("Processing");
+			addMessage("Processing...")
 
 			const formData = new FormData();
 			formData.append('audio', e.data, 'recording.webm');
@@ -113,29 +141,27 @@ document.body.addEventListener('contextmenu', function(e) {
 			})
 			.then((response) => (response.json()))
 			.then((dataStr) => {
-				userAction = dataStr.text;
+				let userAction = dataStr.text;
 
-				updateTXT(userAction);
-				chatBox.scrollTop = chatBox.scrollHeight;
+				addMessage(userAction)
 
-				aiChatUI();
-				updateTXT("Processing");
-				chatBox.scrollTop = chatBox.scrollHeight;
+				addMessage("Processing", from="ai", createNew=true)
 
 				// GPT
-				const formData2 = new FormData();
-				formData2.append('string1', userAction);
-				formData2.append('string2', textVal);
+				let formDataGPT = new FormData();
+				formDataGPT.append('string1', userAction);
+				if (userQuery) {
+					formDataGPT.append('string2', userQuery);
+				}
 				
 				fetch('https://e6ce-2604-3d08-6080-5500-8b6c-a62c-b485-c757.ngrok-free.app/gpt', {
 					method: 'POST',
-					body: formData2,
+					body: formDataGPT,
 				})
 				.then((response) => (response.json()))
 				.then((dataStr) => {
-					updateTXT(dataStr.text);
-					chatBox.scrollTop = chatBox.scrollHeight;
-					micActivated = false;
+					addMessage(dataStr.text);
+					// micActivated = false;
 				})
 				.catch((err) => console.log(err));
 			})
@@ -149,73 +175,3 @@ document.body.addEventListener('contextmenu', function(e) {
 	})
 	.catch((err) => console.log(err))
 });
-
-function audio() {
-	let userAction = "";
-
-	if (document.getElementById("chatBoxWrapper") == null) {
-		document.body.appendChild(chatBoxWrapper);
-		console.log(chatBox.style.display);
-	}
-
-	navigator.mediaDevices.getUserMedia({ audio: true, })
-	.then((stream) => {
-		console.log(stream);
-		mediaRecorder = new MediaRecorder(stream);
-		mediaRecorder.start();
-		micActivated = true;
-
-		// Displaying
-		userChatUI();
-		viewBtn.style.visibility = "visible";
-		micBtn.style.visibility = "visible";
-		updateTXT("Recording");
-		chatBox.scrollTop = chatBox.scrollHeight;
-
-		mediaRecorder.ondataavailable = e => {
-			
-			updateTXT("Processing");
-
-			const formData = new FormData();
-			formData.append('audio', e.data, 'recording.webm');
-			fetch('https://e6ce-2604-3d08-6080-5500-8b6c-a62c-b485-c757.ngrok-free.app/audio', {
-				method: 'POST',
-				body: formData,
-			})
-			.then((response) => (response.json()))
-			.then((dataStr) => {
-				userAction = dataStr.text;
-
-				updateTXT(userAction);
-				chatBox.scrollTop = chatBox.scrollHeight;
-
-				aiChatUI();
-				updateTXT("Processing");
-				chatBox.scrollTop = chatBox.scrollHeight;
-
-				// GPT
-				const formData2 = new FormData();
-				formData2.append('string1', userAction);
-				
-				fetch('https://e6ce-2604-3d08-6080-5500-8b6c-a62c-b485-c757.ngrok-free.app/gpt', {
-					method: 'POST',
-					body: formData2,
-				})
-				.then((response) => (response.json()))
-				.then((dataStr) => {
-					updateTXT(dataStr.text);
-					chatBox.scrollTop = chatBox.scrollHeight;
-					micActivated = false;
-				})
-				.catch((err) => console.log(err));
-			})
-			.catch((err) => console.log(err));
-		};
-
-		setTimeout(() => {
-			mediaRecorder.stop();
-			stream.getTracks().forEach(t => t.stop());
-		}, 3000);
-	})
-	.catch((err) => console.log(err))
-}
